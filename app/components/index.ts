b22806e9b3fddfc6cf4aa4bfc44278c38cbebdb6
Md Anjum Kamali <mdanjumkamali@gmail.com>
2023-12-09 20:03:08 +0530

    Refactor: Organise import statement

---
+++ app/components/index.ts
@@ -0,0 +1,7 @@
+import Link from "./Link";
+import IssueStatusBadge from "./IssueStatusBadge";
+import ErrorMessage from "./ErrorMessage";
+import Spiner from "./Spiner";
+import Skeleton from "./Skeleton";
+
+export { Link, IssueStatusBadge, ErrorMessage, Spiner, Skeleton };
