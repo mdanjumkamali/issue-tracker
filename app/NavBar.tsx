25d1a17c599b5e993dbc73c7d19cc35c2f34ff98
Md Anjum Kamali <mdanjumkamali@gmail.com>
2023-12-11 00:21:01 +0530

    Fix avatar not loading

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -56,6 +56,7 @@ export const NavBar = () => {
                     size="2"
                     radius="full"
                     className="cursor-pointer"
+                    referrerPolicy="no-referrer"
                   />
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
